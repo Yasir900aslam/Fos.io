7513e1236a175c31113518a06f5c4774076c6b8b
Yasir Aslam <yasir900aslam@gmail.com>
2021-07-21 02:23:42 +0500

    Header improvements

--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,30 @@
 import React from "react";
-import { Grid, Text, Button, Divider } from "@geist-ui/react";
+import {
+  Grid,
+  Text,
+  Button,
+  Divider,
+  Spacer,
+  Avatar,
+  Description,
+} from "@geist-ui/react";
 export default function Header() {
   return (
     <>
       <Grid.Container gap={2}>
         <Grid xs={12}>
-          <Text>File.io</Text>
+          <Text h1>Fos.io</Text>
         </Grid>
-        <Grid xs={12}>
-          <Button>Login</Button>
-          <Button>Sign up</Button>
+        <Grid xs={12} style={{ marginTop: "20px" }}>
+          <Button size="small" shadow type="secondary">
+            Login
+          </Button>
+          <Spacer x={1} />
+          <Button size="small" type="default">
+            Sign up
+          </Button>
+          <Spacer x={5} />
+          <Avatar text="W" />
         </Grid>
       </Grid.Container>
       <Divider />
