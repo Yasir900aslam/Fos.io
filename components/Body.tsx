ad9e1ad7898a37f8149ec1d301ef970dfaf10288
Yasir Aslam <yasir900aslam@gmail.com>
2021-07-21 03:12:14 +0500

    First Prototype is ready

--- components/Body.tsx
+++ components/Body.tsx
@@ -56,7 +56,6 @@ export default function Body() {
           </Row>
         </Grid>
       </Grid.Container>
-      <Divider />
     </>
   );
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,53 @@
 import React from "react";
-
+import NextLink from "next/link";
+import { Row, Col, Grid, Divider, Link } from "@geist-ui/react";
 export default function Footer() {
-  return <>Footer</>;
+  return (
+    <>
+      <div style={{ marginLeft: "15px", marginTop: "50px" }}>
+        <Divider />
+        <Grid.Container gap={1}>
+          <Grid xs={8} direction="column">
+            <Row>
+              <Col>
+                <NextLink href="/en-us/components/button">
+                  <Link block>Github</Link>
+                </NextLink>
+              </Col>
+            </Row>
+            <Row>
+              <NextLink href="/en-us/components/button">
+                <Link block>LinkedIn</Link>
+              </NextLink>
+            </Row>
+            <Row>
+              <Col>
+                <NextLink href="/en-us/components/button">
+                  <Link block>Buy me a coffee </Link>
+                </NextLink>
+              </Col>
+            </Row>
+          </Grid>
+          <Grid xs={8}>
+            <Row>
+              <Col>
+                <NextLink href="/en-us/components/button">
+                  <Link block>Developers API</Link>
+                </NextLink>
+              </Col>
+            </Row>
+          </Grid>
+          <Grid xs={8}>
+            <Row>
+              <Col>
+                <NextLink href="/en-us/components/button">
+                  <Link block>Documentation</Link>
+                </NextLink>
+              </Col>
+            </Row>
+          </Grid>
+        </Grid.Container>
+      </div>
+    </>
+  );
 }
